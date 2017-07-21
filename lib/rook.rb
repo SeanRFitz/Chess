56d5ea2537d0c964a8b619387d9dc0007ee3d780
Sean Fitzgerald <Sean.R.Fitzgerald@outlook.com>
2017-07-20 20:39:58 -0500

    Completes the move method for the rook class and passes tests.

--- lib/rook.rb
+++ lib/rook.rb
@@ -1,6 +1,12 @@
 require_relative 'chess_piece.rb'
 
 class Rook < Piece
+	attr_accessor :moved
+
+	def initialize(player,file,rank)
+		super(player,file,rank)
+		@moved = false
+	end
 
 	def display
 		if @player == :white
@@ -8,5 +14,52 @@ class Rook < Piece
 		else
 			print "\u{2656}"
 		end
+	end
+
+	def move
+		new_square, new_file, new_rank = get_input
+		if valid_input?(new_square,rank)
+			if possible_move?(new_file,new_rank)
+				return new_file, new_rank	
+			else
+				puts "Not a valid move to #{(new_file + 96).chr.upcase + new_rank.to_s}"
+			end
+		end
+		false
+	end
+
+	private
+	def get_input
+		puts "Where do you want to move the #{(@file + 96).chr.upcase + @rank.to_s} rook? "
+		new_square = gets.chomp.downcase
+		new_file = new_square[0].ord - 96
+		new_rank = new_square[1].to_i
+		return new_square, new_file, new_rank
+	end
+
+	def valid_input?(new_square,rank)
+		files = ["a","b","c","d","e","f","g","h"]
+		ranks = [1,2,3,4,5,6,7,8]
+		if new_square.length != 2
+			puts "Enter only a letter (A-H) and a number (1-8)."
+		elsif !files.include?(new_square[0])
+			puts "Only files A-H are valid."
+		elsif !ranks.include?(rank)
+			puts "Only ranks 1-8 are valid."
+		else
+			return true
+		end
+		false
+	end
+
+	def possible_move?(new_file,new_rank)
+		if @file == new_file && @rank == new_rank
+			puts "Please pick a square the piece does not already occupy"
+			return false
+		elsif @file == new_file || @rank == new_rank
+			return true
+		end
+		puts "Rooks can only move in non-diagonal straight lines."
+		false
 	end
 end
--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -7,29 +7,76 @@ describe Rook do
 	let(:r2b) { Rook.new(:black,8,8) }
 
 	describe "#initialize" do
-		context "white" do
-			it "creates a rook at A1" do
+		context "creates a white rook" do
+			it "at A1" do
 				expect(r1.player).to eql(:white)
 				expect(r1.file).to eql(1)
 				expect(r1.rank).to eql(1)
+				expect(r1.moved).to eql(false)
 			end
-			it "creates a rook at H1" do
+			it "at H1" do
 				expect(r2.player).to eql(:white)
 				expect(r2.file).to eql(8)
 				expect(r2.rank).to eql(1)
+				expect(r2.moved).to eql(false)
 			end
 		end
 
-		context "black" do
-			it "creates a rook at A8" do
+		context "creates a black rook" do
+			it "A8" do
 				expect(r1b.player).to eql(:black)
 				expect(r1b.file).to eql(1)
 				expect(r1b.rank).to eql(8)
+				expect(r1b.moved).to eql(false)
 			end
-			it "creates a rook at H8" do
+			it "H8" do
 				expect(r2b.player).to eql(:black)
 				expect(r2b.file).to eql(8)
 				expect(r2b.rank).to eql(8)
+				expect(r2b.moved).to eql(false)
+			end
+		end
+	end
+
+	describe ".move" do
+		context "a white rook at A1" do
+			it "returns the rank and file for a valid move to A6" do
+				allow(r1).to receive(:gets).and_return("a6")
+				expect(r1.move).to eql([1,6])
+			end
+			it "returns false for an invalid move to C3" do
+				allow(r1).to receive(:gets).and_return("c3")
+				expect(r1.move).to eql(false)
+			end
+		end
+		context "a white rook at H1" do
+			it "returns the rank and file for a valid move to B1" do
+				allow(r2).to receive(:gets).and_return("b1")
+				expect(r2.move).to eql([2,1])
+			end
+			it "returns false for an invalid move to F7" do
+				allow(r2).to receive(:gets).and_return("f7")
+				expect(r2.move).to eql(false)
+			end
+		end
+		context "a black rook at A8" do
+			it "returns the rank and file for a valid move to A6" do
+				allow(r1b).to receive(:gets).and_return("a6")
+				expect(r1b.move).to eql([1,6])
+			end
+			it "returns false for an invalid move to C3" do
+				allow(r1b).to receive(:gets).and_return("c3")
+				expect(r1b.move).to eql(false)
+			end
+		end
+		context "a black rook at H8" do
+			it "returns the rank and file for a valid move to B8" do
+				allow(r2b).to receive(:gets).and_return("b8")
+				expect(r2b.move).to eql([2,8])
+			end
+			it "returns false for an invalid move to F7" do
+				allow(r2b).to receive(:gets).and_return("f7")
+				expect(r2b.move).to eql(false)
 			end
 		end
 	end
