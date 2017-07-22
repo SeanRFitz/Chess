827a00b374b0365d707c9bd4c94b1f41377787cb
Sean Fitzgerald <Sean.R.Fitzgerald@outlook.com>
2017-07-21 19:58:07 -0500

    Creates move and display method for Bishop class and passes tests.

--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -34,4 +34,59 @@ describe Bishop do
 			end
 		end
 	end
+
+	describe ".move" do
+		let (:e4bishop) { Bishop.new(:white,5,4) }
+		context "a white bishop at C1" do
+			it "returns [2,2] when it moves to B2" do
+				allow(b1).to receive(:gets).and_return("b2")
+				expect(b1.move).to eql([2,2])
+			end
+			it "returns false when it attempts to move to A1" do
+				allow(b1).to receive(:gets).and_return("a1")
+				expect(b1.move).to eql(false)
+			end
+			it "returns false when it receives invalid input Z8" do
+				allow(b1).to receive(:gets).and_return("z8")
+				expect(b1.move).to eql(false)	
+		end	end
+		context "a white bishop at E4" do
+			
+
+			it "returns [8,1] when it moves to H1" do
+				allow(e4bishop).to receive(:gets).and_return("h1")
+				expect(e4bishop.move).to eql([8,1])
+			end
+			it "returns [2,7] when it moves to B7" do
+				allow(e4bishop).to receive(:gets).and_return("b7")
+				expect(e4bishop.move).to eql([2,7])
+			end
+			it "returns [7,6] when it moves to G6" do
+				allow(e4bishop).to receive(:gets).and_return("g6")
+				expect(e4bishop.move).to eql([7,6])
+			end
+			it "returns [4,3] when it moves to D3" do
+				allow(e4bishop).to receive(:gets).and_return("d3")
+				expect(e4bishop.move).to eql([4,3])
+			end
+			it "returns false when it attempts to move to E8" do
+				allow(e4bishop).to receive(:gets).and_return("e8")
+				expect(e4bishop.move).to eql(false)
+			end
+			it "returns false when it attempts to move to F8" do
+				allow(e4bishop).to receive(:gets).and_return("f8")
+				expect(e4bishop.move).to eql(false)
+			end
+			it "returns false when it attempts to move to A5" do
+				allow(e4bishop).to receive(:gets).and_return("a5")
+				expect(e4bishop.move).to eql(false)
+			end
+			it "returns false when it attempts to move to G4" do
+				allow(e4bishop).to receive(:gets).and_return("g4")
+				expect(e4bishop.move).to eql(false)
+			end
+
+		end
+	end
+
 end
